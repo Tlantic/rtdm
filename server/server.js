194f157daa1d8a62aa4c3031cd1c15767799daff
RAA <oi@renatoabrahao.com>
2014-11-18 15:58:33 -0200

    Publishing HTML5 app on /cordova for local testing

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ router.loadAPI('/app/nbapi/routes');
 
 // publishing studio
 router.app.use('/studio', require('express').static(__dirname + "/public"));
+router.app.use('/cordova', require('express').static(__dirname + "/../mobile/cordova/src"));
 
 
 // ***** Start the app by listening on <port> *****
