b3ad37f139aef73fac1c5892846094e765f31a67
Tiago Padilha <padilha@sinos.net>
2014-11-20 21:51:50 -0200

    Removed Android's build files.

--- mobile/cordova/rtdmma/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\tpadilha\Github\rtdm\mobile\cordova\rtdmma\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\tpadilha\Github\rtdm\mobile\cordova\rtdmma\platforms\android\CordovaLib\AndroidManifest.xml \
--- mobile/cordova/rtdmma/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-c:\Users\tpadilha\Github\rtdm\mobile\cordova\rtdmma\platforms\android\ant-build\AndroidManifest.xml : \
-c:\Users\tpadilha\Github\rtdm\mobile\cordova\rtdmma\platforms\android\AndroidManifest.xml \
