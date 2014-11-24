3e1862d6d6d69505ccc90b7a9a641e37be3e24f7
RAA <oi@renatoabrahao.com>
2014-11-24 18:01:46 -0200

    Repo cleanup: mobile...ant-build

--- mobile/cordova/rtdmma/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/padilha/Developer/Tlantic/rtdm/mobile/cordova/rtdmma/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/padilha/Developer/Tlantic/rtdm/mobile/cordova/rtdmma/platforms/android/AndroidManifest.xml \
--- mobile/cordova/rtdmma/platforms/android/ant-build/TlanticRealTimeDeliverManager-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/padilha/Developer/Tlantic/rtdm/mobile/cordova/rtdmma/platforms/android/ant-build/TlanticRealTimeDeliverManager-debug-unaligned.apk : \
-/Users/padilha/Developer/Tlantic/rtdm/mobile/cordova/rtdmma/platforms/android/ant-build/TlanticRealTimeDeliverManager.ap_ \
-/Users/padilha/Developer/Tlantic/rtdm/mobile/cordova/rtdmma/platforms/android/ant-build/classes.dex \
-/Users/padilha/Developer/Tlantic/rtdm/mobile/cordova/rtdmma/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/padilha/Developer/Tlantic/rtdm/mobile/cordova/rtdmma/platforms/android/libs/com.google.zxing.client.android.captureactivity.jar \
