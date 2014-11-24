2f9697f1a2c58a50ccf629538cb3e96f79e1f482
RAA <oi@renatoabrahao.com>
2014-11-24 17:19:26 -0200

    Repo cleanup: www

---
+++ mobile/cordova/rtdmma/src/js/app_.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var rtdmma = angular.module('rtdmma', [
+		"ngRoute",
+		"ngResource",
+		"ngTouch",
+		"rtdmmaFilters",
+		"rtdmmaServices",
+		"rtdmmaControllers",
+		"mobile-angular-ui"
+	]);
+
+rtdmma.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/login', {templateUrl: 'login_.html', controller: 'LoginCtrl'}).
+		when('/logout', {templateUrl: 'logout_.html', controller: 'LogoutCtrl'}).
+		when('/users', {templateUrl: 'user-list_.html', controller: 'UserListCtrl'}).
+		when('/users/:userId', {templateUrl: 'user-tasks_.html', controller: 'UserTasksListCtrl'}).
+		when ('/task/:taskId', {templateUrl: 'task-detail_.html', controller: 'TaskCtrl'}).
+		otherwise({redirectTo: '/login'});
+}]);
